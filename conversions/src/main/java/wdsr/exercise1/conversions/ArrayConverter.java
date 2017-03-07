d9c4d1040b06135cd7f27958a81a361c57ed3e8a
Blaszczyk <marcin.blaszczyk@accenture.com>
2017-03-07 09:16:04 +0100

    zadanie 1c

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,33 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+	private static final Logger log = LogManager.getLogger();
+	
+	
     public int[] convertToInts(String[] strings) {
-        return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+    	log.info("convertToInts method with param strings ={");
+    	for(String value: strings){
+    		log.info(value+", ");
+    	}
+    	log.info("}");
+    	
+        int[] result = Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
+        
+        log.info("Return of the method {");
+    	for(int value: result){
+    		log.info(value+", ");
+    	}
+        log.info("}");
+        return result;
     }
 }
